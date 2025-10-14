c13c48dfbe5022e618a06986037e48905c6e5103
Nouman Ashfaq <noumanishfaq93@gmail.com>
2025-10-14 05:05:39 +0500

    Changes done for the firebase notifcation

--- src/main/java/com/appointment/config/FirebaseConfig.java
+++ src/main/java/com/appointment/config/FirebaseConfig.java
@@ -1,33 +1,33 @@
-package com.appointment.config;
-
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import jakarta.annotation.PostConstruct;
-import org.springframework.context.annotation.Configuration;
-
-import java.io.InputStream;
-
-@Configuration
-public class FirebaseConfig {
-
-    @PostConstruct
-    public void initialize() {
-        try {
-            InputStream serviceAccount = getClass()
-                    .getClassLoader()
-                    .getResourceAsStream("lawyers-appointment-firebase.json");
-
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
-            }
-
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to initialize Firebase", e);
-        }
-    }
-}
+//package com.appointment.config;
+//
+//import com.google.auth.oauth2.GoogleCredentials;
+//import com.google.firebase.FirebaseApp;
+//import com.google.firebase.FirebaseOptions;
+//import jakarta.annotation.PostConstruct;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.io.InputStream;
+//
+//@Configuration
+//public class FirebaseConfig {
+//
+//    @PostConstruct
+//    public void initialize() {
+//        try {
+//            InputStream serviceAccount = getClass()
+//                    .getClassLoader()
+//                    .getResourceAsStream("lawyers-appointment-firebase.json");
+//
+//            FirebaseOptions options = new FirebaseOptions.Builder()
+//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                    .build();
+//
+//            if (FirebaseApp.getApps().isEmpty()) {
+//                FirebaseApp.initializeApp(options);
+//            }
+//
+//        } catch (Exception e) {
+//            throw new RuntimeException("Failed to initialize Firebase", e);
+//        }
+//    }
+//}
