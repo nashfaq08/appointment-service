79230833d7ea377ba2a6fe09bda9d7cb6955c7f1
Nouman Ashfaq <noumanishfaq93@gmail.com>
2025-11-04 14:51:33 +0500

    Changes done for the code refactoring

--- src/main/java/com/appointment/repositories/DocumentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.appointment.repositories;
-
-import com.appointment.entities.Document;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocumentRepository extends JpaRepository<Document, Long> {
-}
-
-
